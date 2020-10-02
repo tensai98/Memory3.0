8eb558c9b4131dba904e501ef1a8ac8e0badda07
Chi Joung So <chijoung@gmail.com>
2020-10-03 00:11:02 +0200

    refactor Cell

--- src/main/java/com/game/memory/stupid/parts/MemoryPane.java
+++ src/main/java/com/game/memory/stupid/parts/MemoryPane.java
@@ -2,10 +2,7 @@ package com.game.memory.stupid.parts;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,11 +13,12 @@ public class MemoryPane extends GridPane {
 
     private static Logger log = LoggerFactory.getLogger(MemoryPane.class);
 
+    private static final String noBorderStyle = "-fx-border-color: white; -fx-border-width: 0px";
+    private static final String blueBorderStyle = "-fx-border-color: skyblue; -fx-border-width: 5px";
+
     private int[][] board;
-    private String cardType;
     private Long startTime;
     private Cell[][] cell;
-    private ImageView imageView;
     private boolean first = true;
     private int numberOfCouples;
     private int i1;
@@ -34,9 +32,25 @@ public class MemoryPane extends GridPane {
     public MemoryPane(ScoreBox scoreBox, int numberOfCouples, String cardType) {
         this.scoreBox = scoreBox;
         this.numberOfCouples = numberOfCouples;
-        this.cardType = cardType;
-        this.setAlignment(Pos.CENTER); // set center
-        this.setStyle("-fx-background-color: antiquewhite");
+
+        setAlignment(Pos.CENTER); // set center
+        setStyle("-fx-background-color: antiquewhite");
+        setPadding(new Insets(5, 5, 5, 5));
+        setHgap(15);
+        setVgap(15);
+
+        randomizeBoard();
+
+        displayBoard(cardType);
+
+        startTime = System.currentTimeMillis();
+
+        if (checkEndGame()) {
+            log.info("Game over!!");
+        }
+    }
+
+    private void randomizeBoard() {
         // creeer matrix board met integers
         int numberRows = (int) Math.sqrt(2 * numberOfCouples);
         int numberColumns = (2 * numberOfCouples) / numberRows;
@@ -50,34 +64,18 @@ public class MemoryPane extends GridPane {
             }
         }
         shuffleCards();
-
-        setPadding(new Insets(5, 5, 5, 5));
-        setHgap(15);
-        setVgap(15);
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                add(cell[i][j] = new Cell(i, j), i, j);
-                this.setAlignment(Pos.CENTER);
-            }
-        }
-
-        startTime = System.currentTimeMillis();
-
-        if (checkEndGame())
-            log.info("Game over!!");
-
     }
 
     private boolean checkEndGame() {
         return count == numberOfCouples;
     }
 
-    private void displayBoard() {
+    private void displayBoard(String cardType) {
         this.getChildren().clear();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                add(cell[i][j], i, j);
+                add(cell[i][j] = new Cell(this, cardType, i, j, board[i][j]), i, j);
+                this.setAlignment(Pos.CENTER);
             }
         }
     }
@@ -94,110 +92,66 @@ public class MemoryPane extends GridPane {
         }
     }
 
-    public class Cell extends StackPane {
-        private boolean flipped;
-        private int i;
-        private int j;
+    public void handleMouseClick(int i, int j) {
+        scoreBox.startTimer();
+        if (first) {
+            // not flipped yet
+            if (!cell[i][j].isFlipped()) {
 
-        public Cell(int i, int j) {
-            this.i = i;
-            this.j = j;
-
-            this.setPrefSize(200, 150);
-            this.setOnMouseClicked(e -> handleMouseClick());
-
-            if (!flipped) {
-                setBack();
-            } else {
-                setFront();
-            }
-        }
-
-        private void setBack() {
-            imageView = new ImageView(new Image("/image/" + cardType
-                    + "/backCard.jpg"));
-            this.getChildren().add(imageView);
-        }
-
-        private void setFront() {
-            imageView = new ImageView(new Image("/image/" + cardType + "/"
-                    + board[getI()][getJ()] + ".jpg"));
-            this.getChildren().add(imageView);
-        }
-
-        public boolean isFlipped() {
-            return flipped;
-        }
-
-        private void handleMouseClick() {
-            scoreBox.startTimer();
-            if (first) {
-                if (!flipped) {
-                    if (!cell[i1][j1].flipped)
-                        cell[i1][j1].setBack();
-                    if (!cell[i2][j2].flipped)
-                        cell[i2][j2].setBack();
-
-                    cell[getI()][getJ()].flipped = true;
-                    setStyle("-fx-border-color: skyblue; -fx-border-width: 5px");
-                    i1 = getI();
-                    j1 = getJ();
-                    setFront();
-                    first = false;
-                } else
-                    log.info("Pick another cards");
-            } else {
-                if (!flipped) {
-                    i2 = getI();
-                    j2 = getJ();
-                    cell[i2][j2].flipped = true;
-                    setStyle("-fx-border-color: skyblue; -fx-border-width: 5px");
-                    setFront();
-                    if (match(i1, j1, i2, j2)) {
-                        scoreBox.addTry("Match!");
-
-                        count++;
-                        // This part is a self check, will be replaced
-                        log.info("Count is " + count);
-                        if (checkEndGame()) {
-                            log.info("Game is over");
-                            log.info("Time taken: " + (System.currentTimeMillis() - startTime));
-                            scoreBox.endGame();
-                            setScore();
-                        }
-
-                    } else {
-                        scoreBox.addTry("No match!");
-                        cell[i1][j1].flipped = false;
-                        cell[i2][j2].flipped = false;
-                        //startTime -= 3000;		// penalty for wrong guess
-                    }
-                    first = true;
-                    cell[i1][j1].setStyle("-fx-border-color: white; -fx-border-width: 0px");
-                    this.setStyle("-fx-border-color: white; -fx-border-width: 0px");
-                } else {
-                    log.info("Pick another cards");
-                    cell[i2][j2].flipped = false;
+                // reset cards on first pick
+                if (!cell[i1][j1].isFlipped()) {
+                    cell[i1][j1].setBack();
                 }
+                if (!cell[i2][j2].isFlipped()) {
+                    cell[i2][j2].setBack();
+                }
+
+                cell[i][j].setFlipped(true);
+                cell[i][j].setStyle(blueBorderStyle);
+                cell[i][j].setFront();
+
+                i1 = i;
+                j1 = j;
+                first = false;
+            } else {
+                log.info("Pick another cards");
+            }
+        } else {
+            if (!cell[i][j].isFlipped()) {
+                i2 = i;
+                j2 = j;
+                cell[i][j].setFlipped(true);
+                cell[i][j].setStyle(blueBorderStyle);
+                cell[i][j].setFront();
+                if (match(i1, j1, i2, j2)) {
+                    scoreBox.addTry("Match!");
+
+                    count++;
+                    // This part is a self check, will be replaced
+                    log.info("Count is " + count);
+                    if (checkEndGame()) {
+                        log.info("Game is over! Time taken: {}.", (System.currentTimeMillis() - startTime));
+                        scoreBox.endGame();
+                     }
+
+                } else {
+                    scoreBox.addTry("No match!");
+                    cell[i1][j1].setFlipped(false);
+                    cell[i2][j2].setFlipped(false);
+                    //startTime -= 3000;		// penalty for wrong guess
+                }
+                first = true;
+                cell[i1][j1].setStyle(noBorderStyle);
+                cell[i2][j2].setStyle(noBorderStyle);
+            } else {
+                log.info("Pick another card");
+                cell[i2][j2].setFlipped(false);
             }
         }
+    }
 
-        private void setScore() {
-            // needs to implemented
-        }
-
-        private boolean match(int i1, int j1, int i2, int j2) {
-            log.info("Checking if value '{}' is equal to '{}'.", board[i1][j1], board[i2][j2]);
-            return board[i1][j1] == board[i2][j2];
-        }
-
-        public int getI() {
-            return i;
-        }
-
-        public int getJ() {
-            return j;
-        }
-
+    private boolean match(int i1, int j1, int i2, int j2) {
+        log.info("Checking if value '{}' is equal to '{}'.", board[i1][j1], board[i2][j2]);
+        return board[i1][j1] == board[i2][j2];
     }
 }
